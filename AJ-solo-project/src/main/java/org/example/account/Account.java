f057c9c454605c05656e33524c5259218a66c06f
4yourztruly <4yourztruly@gmail.com>
2024-10-26 11:39:21 +0200

    Started adding methods and variables to account, accountmanager and transaction

--- AJ-solo-project/src/main/java/org/example/account/Account.java
+++ AJ-solo-project/src/main/java/org/example/account/Account.java
@@ -1,4 +1,46 @@
 package org.example.account;
 
+import java.util.ArrayList;
+
 public class Account {
+    private final String username;
+    private final String password;
+    private final ArrayList<Transaction>transactions = new ArrayList<>();
+    private int balance;
+
+    public Account(String username, String password) {
+        this.username = username;
+        this.password = password;
+        this.balance = 0;
+    }
+
+    public void addTransaction(Transaction transaction) {
+        transactions.add(transaction);
+    }
+
+    public void removeTransaction(Transaction transaction) {
+        transactions.remove(transaction);
+    }
+
+    public String getUsername() {
+        return username;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public ArrayList<Transaction> getTransactions() {
+        return transactions;
+    }
+
+    public int getBalance() {
+        return balance;
+    }
+
+    public void addBalance(int balance) {
+        this.balance += balance;
+    }
+
+
 }
--- AJ-solo-project/src/main/java/org/example/account/Transaction.java
+++ AJ-solo-project/src/main/java/org/example/account/Transaction.java
@@ -1,4 +1,9 @@
 package org.example.account;
 
+import java.time.LocalDate;
+
 public class Transaction {
+    private String name;
+    private int price;
+    private LocalDate date;
 }

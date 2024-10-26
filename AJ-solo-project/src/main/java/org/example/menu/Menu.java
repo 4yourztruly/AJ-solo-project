5fff63705383d414cad083544e259618f2aee0e0
4yourztruly <4yourztruly@gmail.com>
2024-10-26 13:24:07 +0200

    added more methods in command and added integration to mainmenu

--- AJ-solo-project/src/main/java/org/example/menu/Menu.java
+++ AJ-solo-project/src/main/java/org/example/menu/Menu.java
@@ -44,11 +44,61 @@ public class Menu {
     }
 
     public static void signupMenu() {
-        
+        System.out.println();
+        System.out.println("Enter your username: ");
+        String username = scanner.nextLine();
+        System.out.println("Enter your password: ");
+        String password = scanner.nextLine();
+        Account account = new Account(username,password);
+        AccountManager.addAccount(account);
+        System.out.println("Added " + account.getUsername());
+        Menu.greetMenu();
     }
 
     public static void mainMenu(Account account) {
         System.out.println("Hello " + account.getUsername() + " Welcome to Personal Finance!");
         System.out.println("Your balance is " + account.getBalance() + "kr");
+        System.out.println("Enter help for commands!");
+        String command = scanner.nextLine();
+        if(command.equalsIgnoreCase("help")) {
+            System.out.println("View transactions = vt, add transactions = at, remove transaction = rt, view history in year, month, week or day = vy, vm, vw, vd, Add balance = ab, Logout = logout");
+        }
+
+        if(command.equalsIgnoreCase("vt")) {
+            Command.viewTransactions(account);
+        }
+
+        if(command.equalsIgnoreCase("at")) {
+            Command.addTransaction(account);
+        }
+
+        if(command.equalsIgnoreCase("rt")) {
+            Command.removeTransaction(account);
+        }
+
+        if(command.equalsIgnoreCase("vy")) {
+            Command.viewYear();
+        }
+
+        if(command.equalsIgnoreCase("vm")) {
+            Command.viewMonth();
+        }
+
+        if(command.equalsIgnoreCase("vw")) {
+            Command.viewWeek();
+        }
+
+        if(command.equalsIgnoreCase("vd")) {
+            Command.viewDay();
+        }
+
+        if(command.equalsIgnoreCase("ab")) {
+            Command.addBalance(account);
+        }
+
+        if(command.equalsIgnoreCase("logout")) {
+            System.out.println("Logging you out!");
+            greetMenu();
+        }
     }
 }
